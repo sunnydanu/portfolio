d72e49d48e44c0bc1ace50a23708a7d9abba74e6
sunny danu <mail@sunnydanu.com>
2023-09-24 20:57:52 +0530

    number removed

--- src/portfolio.js
+++ src/portfolio.js
@@ -337,7 +337,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
+  number: "",
   email_address: "mail@sunnydanu.com"
 };
 
